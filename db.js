04c189ea723812877b629043695e0e159d2463fb
royalrana82196-alt <royalrana974@gmail.com>
2025-09-02 15:00:39 +0530

    chnages shift mongo db to online server1

--- db.js
+++ db.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-
-// const mongoURL = "mongodb://localhost:27017/newhotels"
+ const mongoURL = "mongodb://localhost:27017/newhotels"
 // const mongoURL = "mongodb+srv://royalrana974_db_user:mgsjaf226zLGfNYN@cluster0.nnom3if.mongodb.net/"
-// const mongoURL = process.env.MONGODB_URL_LOCAL;
-const mongoURL = process.env.MONGODB_URL;
-
 
 mongoose.connect("mongodb://localhost:27017/newhotels")
   .then(() => console.log("MongoDB connected!!!"))
@@ -14,3 +10,20 @@ mongoose.connect("mongodb://localhost:27017/newhotels")
 const db = mongoose.connection;
 
 module.exports = db;
+
+
+// const mongoose = require("mongoose");
+// require('dotenv').config();
+
+// // const mongoURL = "mongodb://localhost:27017/newhotels"
+// const mongoURL = "mongodb+srv://royalrana974_db_user:mgsjaf226zLGfNYN@cluster0.nnom3if.mongodb.net/"
+ 
+// mongoose.connect(mongoURL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+//   .then(() => console.log("MongoDB connected!!"))
+//   .catch(err => console.error("MongoDB connection error:", err));
+// const db = mongoose.connection;
+
+// module.exports = db;
