6dbdcd43ceed32769d5a53de3f07b2af0379e910
royalrana82196-alt <royalrana974@gmail.com>
2025-09-02 15:22:04 +0530

    chnages shift mongo db to online server1

--- db.js
+++ db.js
@@ -1,32 +1,11 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
 
-const mongoURL = "mongodb://127.0.0.1:27017/newhotels"; // use 127.0.0.1 instead of localhost
+// const mongoURL = "mongodb://localhost:27017/newhotels"
+const mongoURL = "mongodb+srv://royalrana974_db_user:mgsjaf226zLGfNYN@cluster0.nnom3if.mongodb.net/"
 
-mongoose.connect(mongoURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("✅ MongoDB connected!!!"))
-.catch(err => console.error("❌ MongoDB connection error:", err));
-
+mongoose.connect("mongodb://localhost:27017/newhotels")
+  .then(() => console.log("MongoDB connected!!!"))
+  .catch(err => console.error("MongoDB connection error:", err));
 const db = mongoose.connection;
+
 module.exports = db;
-
-
-
-// const mongoose = require("mongoose");
-// require('dotenv').config();
-
-// // const mongoURL = "mongodb://localhost:27017/newhotels"
-// const mongoURL = "mongodb+srv://royalrana974_db_user:mgsjaf226zLGfNYN@cluster0.nnom3if.mongodb.net/"
- 
-// mongoose.connect(mongoURL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-//   .then(() => console.log("MongoDB connected!!"))
-//   .catch(err => console.error("MongoDB connection error:", err));
-// const db = mongoose.connection;
-
-// module.exports = db;
