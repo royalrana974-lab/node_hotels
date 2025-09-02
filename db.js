220b41698d5cd16f4447230b842049ede13451fc
royalrana82196-alt <royalrana974@gmail.com>
2025-09-02 18:10:50 +0530

    chnages shift mongo db to online server

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 
 // const mongoURL = "mongodb://localhost:27017/newhotels"
-// const mongoURL = "mongodb+srv://royalrana974_db_user:mgsjaf226zLGfNYN@cluster0.nnom3if.mongodb.net/"
+// const mongoURL = "mongodb+srv://royalrana974_db_user:foXuHJQMUxgzJKxy@nodeproject.q6s3dhe.mongodb.net/"
 const mongoURL = process.env.MONGODB_URL;
 
 mongoose.connect(mongoURL, {
