9c38f16d76354754dde4d1add69b4c081c8d8b1d
royalrana82196-alt <royalrana974@gmail.com>
2025-09-02 16:41:07 +0530

    chnages shift mongo db to online server1

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ const app = express();
 const db = require('./db');
 require('dotenv').config();
 
-
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
-
 
 const personRoutes = require("./routes/personRoutes");
 app.use('/person', personRoutes)
